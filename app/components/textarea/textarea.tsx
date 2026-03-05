b834b3d00bab6bde1e383b7fc38c1c791d1ef56f
lyla <bgy09270@naver.com>
2026-03-05 17:08:47 +0900

    fix: placeholder 공통 컴포넌트 스타일 및 props 추가

--- app/components/textarea/textarea.tsx
+++ app/components/textarea/textarea.tsx
@@ -5,33 +5,42 @@ import { Textarea as ShadcnTextarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 
 interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label?: string;
   maxLength?: number;
 }
 
-export default function TextArea({ 
-  className, 
-  maxLength = 2000, 
-  value, 
-  onChange, 
-  ...props 
+export default function TextArea({
+  className,
+  maxLength = 2000,
+  value,
+  onChange,
+  label,
+  placeholder,
+  ...props
 }: Props) {
   const currentLength = typeof value === "string" ? value.length : 0;
 
   return (
-    <div className="relative w-full">
+    <div className="relative w-full flex flex-col gap-2">
+      {label && (
+        <label className="text-base font-semibold text-gray-400">{label}</label>
+      )}
       <ShadcnTextarea
         className={cn(
-          "min-h-[200px] w-full resize-none rounded-2xl bg-gray-50 p-4 pb-10 border-none text-[16px] placeholder:text-gray-300 focus-visible:ring-0",
+          "min-h-[200px] w-full resize-none rounded-2xl bg-gray-50 p-4 pb-10 border-none text-[16px] placeholder:text-gray-300 focus-visible:ring-0 shadow-none",
           className
         )}
         maxLength={maxLength}
         value={value}
         onChange={onChange}
+        placeholder={placeholder}
         {...props}
       />
-      
+
       <div className="absolute bottom-4 right-4 text-[12px] text-gray-300">
-        <span className={currentLength > 0 ? "text-gray-300" : ""}>{currentLength}</span>
+        <span className={currentLength > 0 ? "text-gray-300" : ""}>
+          {currentLength}
+        </span>
         <span>/{maxLength}</span>
       </div>
     </div>
